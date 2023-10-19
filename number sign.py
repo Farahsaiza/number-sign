e92d9cba9f326fdb03104c97ff7f0744ebc1774a
Farahsaiza <farahsaiza@gmail.com>
2023-10-19 21:28:11 +0100

    Add files via upload

---
+++ number sign.py
@@ -0,0 +1,8 @@
+A=int(input("enter the first number "))
+B=int(input("enter the second number "))
+if A>0 and B>0  :
+    print("both numbers have the same sign")
+elif A<0 and B<0 :
+    print("both numbers have the same sign")
+else:
+    print("both numbers  don't have the same sign")
